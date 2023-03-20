3d590b85617c52e2c4f6dd76f1e984692485aae2
bogdanborzianytsia <128235605+bogdanborzianytsia@users.noreply.github.com>
2023-03-21 01:35:35 +0200

    Add files via upload

---
+++ Singleton/Singleton/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    using System;
+    using System.Windows.Forms;
+
+    public sealed class DocumentRepository
+    {
+        private static readonly Lazy<DocumentRepository> lazy = new Lazy<DocumentRepository>(() => new DocumentRepository());
+
+        public static DocumentRepository Instance { get { return lazy.Value; } }
+
+        private DocumentRepository() { }
+
+        public void SaveDocument(Document doc, TextBox outputTextbox)
+        {
+            // зберігаємо документ у репозиторії
+            string message = $"Збережено документ з id {doc.Id} у репозиторії.";
+            outputTextbox.AppendText(message + Environment.NewLine);
+        }
+
+        public Document GetDocumentById(int id, TextBox outputTextbox)
+        {
+            // отримуємо документ з репозиторію за id
+            string message = $"Отримано документ з id {id} з репозиторію.";
+            outputTextbox.AppendText(message + Environment.NewLine);
+            return new Document { Id = id, Title = $"Документ #{id}", Content = $"Це текст документа #{id}" };
+        }
+    }
+
+    public class Document
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ Singleton/Singleton/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Singleton
+{
+    public partial class Form1 : Form
+    {
+        private DocumentRepository _documentRepository;
+        public Form1()
+        {
+            InitializeComponent();
+            _documentRepository = DocumentRepository.Instance;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var repo = DocumentRepository.Instance;
+            var outputTextbox = new TextBox();
+
+            var doc1 = new Document { Id = 1, Title = textBox1.Text, Content = textBox2.Text };
+            repo.SaveDocument(doc1, outputTextbox);
+            var savedDoc1 = repo.GetDocumentById(1, outputTextbox);
+            MessageBox.Show(outputTextbox.Text);
+        }
+    }
+}
